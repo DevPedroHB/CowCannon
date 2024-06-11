d16101b6e4c2da97a0c9b444d76d4fee73e44416
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2024-06-11 18:03:32 -0300

    ✨ feat: Permissões

--- src/main/java/dev/pedrohb/cowcannon/Listeners/EntityListener.java
+++ src/main/java/dev/pedrohb/cowcannon/Listeners/EntityListener.java
@@ -8,8 +8,15 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.permissions.PermissionAttachment;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class EntityListener implements Listener {
+
+  private final Map<UUID, PermissionAttachment> permissions = new HashMap<>();
 
   @EventHandler
   public void onEntityRightClick(PlayerInteractEntityEvent event) {
@@ -19,8 +26,28 @@ public class EntityListener implements Listener {
 
     Player player = event.getPlayer();
     Entity entity = event.getRightClicked();
-    
+
+    /*if (permissions.containsKey(player.getUniqueId())) {
+      PermissionAttachment permission = permissions.remove(player.getUniqueId());
+
+      player.removeAttachment(permission);
+
+      player.sendMessage("You no longer have the perm.");
+    } else {
+      PermissionAttachment permission = player.addAttachment(CowCannon.getInstance(), "cowcannon.cow", true);
+
+      permissions.put(player.getUniqueId(), permission);
+
+      player.sendMessage("You now have the perm.");
+    }*/
+
     if (entity instanceof Cow && entity.hasMetadata("CowCannon") && player.getItemInHand().getType() == Material.BUCKET) {
+      if (!player.hasPermission("cowcannon.cow.use")) {
+        player.sendMessage("You don't have permission to milk cows.");
+
+        return;
+      }
+
       Cow cow = (Cow) event.getRightClicked();
 
       cow.getWorld().createExplosion(cow.getLocation(), 2.F);
